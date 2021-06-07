b01e67785257e5bda90459708a7e5f691940e7ff
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-06-07 22:55:43 +0800

    refactored sellBook to 8 lines

--- script.js
+++ script.js
@@ -2,23 +2,14 @@ function store(name, list, earnings) {
   (this.name = name), (this.list = list), (this.earnings = earnings);
 }
 
-//create instance of store
-let sampleStore = new store("Avion Store", [], 0);
-
 function book(title, quantity, value) {
   (this.title = title), (this.quantity = quantity), (this.value = value);
 }
-
-let mybook = new book("Harry Potter", 5, 500);
 
 store.prototype.addBook = function(title, quantity, value){
   let newBook= new book(title, quantity, value)
   this.list.push(newBook);
 }
-
-sampleStore.addBook("Cinder", 10, 300);
-sampleStore.addBook("The Little Prince", 10, 300);
-sampleStore.addBook("Lord of the RIngs", 2, 500);
 
 store.prototype.restockBook = function (title, quantity) {
   this.list.some((book) => {
@@ -29,37 +20,50 @@ store.prototype.restockBook = function (title, quantity) {
   console.log(this.list);
 };
 
-sampleStore.restockBook("Cinder", 5);
-sampleStore.restockBook("Harry Potter", 4);
-
 // TODO
 //sell book
-store.prototype.sellBook = function (title, quantity) {
-  const bookIndex = this.list.findIndex((book) => book.title === title);
+// store.prototype.sellBook = function (title, quantity) {
+//   const bookIndex = this.list.findIndex((book) => book.title === title);
 
-  if (bookIndex !== -1) {
-    const {
-      title: StoreTitle,
-      quantity: Stock,
-      value: Price,
-    } = this.list[bookIndex];
+//   if (bookIndex !== -1) {
+//     const {
+//       title: StoreTitle,
+//       quantity: Stock,
+//       value: Price,
+//     } = this.list[bookIndex];
 
-    if (Stock < quantity) {
-      console.log(`${StoreTitle} has only ${Stock} left`);
-    } else {
-      this.list[bookIndex].quantity -= quantity;
-      this.earnings += quantity * Price;
-    }
-  } else {
-    console.log(`We don't sell that book here`);
-  }
+//     if (Stock < quantity) {
+//       console.log(`${StoreTitle} has only ${Stock} left`);
+//     } else {
+//       this.list[bookIndex].quantity -= quantity;
+//       this.earnings += quantity * Price;
+//     }
+//   } else {
+//     console.log(`We don't sell that book here`);
+//   }
+// };
+
+//refactored - 8 lines
+store.prototype.sellBook = function (bookTitle, bookQuantity) {
+  //destructure this
+  let { name, list, earnings} = this
+  //find specified book
+  const book = list.find((book) => book.title === bookTitle);
+  //return invalid early
+  if(!book) return console.log("We don't sell that book here")
+  //destructure book
+  let {title, quantity, value} = book
+  //return invalid early
+  if(quantity < bookQuantity) return console.log(`${title} has only ${quantity} left`)
+  //main logic for sell
+  quantity -= bookQuantity
+  earnings = bookQuantity * value
+  return console.log(`Sold ${title} successfully`)
 };
 
 store.prototype.totalEarnings = function () {
   console.log(`Store name is ${this.name} with earnings of ${this.earnings}`);
 };
-
-sampleStore.totalEarnings();
 
 store.prototype.listInventory = function () {
   this.list.map((book) => {
@@ -67,4 +71,23 @@ store.prototype.listInventory = function () {
   });
 };
 
+
+//create instance of store
+let sampleStore = new store("Avion Store", [], 0);
+//create books
+sampleStore.addBook("Cinder", 10, 300);
+sampleStore.addBook("The Little Prince", 10, 300);
+sampleStore.addBook("Lord of the RIngs", 2, 500);
+//sell books - happy path
+sampleStore.sellBook("Cinder", 1)
+//sell books - no book condition
+sampleStore.sellBook("Cinder123", 1)
+//sell books - quantitiy insufficient
+sampleStore.sellBook("Cinder", 1000000)
+//restock books
+sampleStore.restockBook("Cinder", 5);
+sampleStore.restockBook("Harry Potter", 4);
+//list earning
+sampleStore.totalEarnings();
+//list inventory
 sampleStore.listInventory();
